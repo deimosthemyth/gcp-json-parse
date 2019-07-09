091b06e92e555fffbb3aff62e3efd8d7a628125a
deimosthemyth <deimos@hacking.film>
2019-07-09 14:40:33 -0500

    Prepping for Output

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # gcp-json-parse
+
 v0.01 Alpha - Parses output of a single well-formatted JSON HTTP GCP data grab to stdout - DONE
+
 v0.02 Alpha - Parse output of a multi-lined JSON HTTP GCP data to txt/csv (haven't decided, input welcome) - IN PROGRESS
 
 Please ignore my garbage coding comments, there is a lot of stuff in there that I will be pulling out and optimizing in the finished product.
--- gcp_http_parse.py
+++ gcp_http_parse.py
@@ -7,7 +7,7 @@ import json
 import base64
 import time
 from urllib import unquote
-#import pprint
+import csv
 
 #Function to define user-supplied input (v2 expand to include what JSON vars they want to output vs static value)
 def userInput():
@@ -20,19 +20,26 @@ def userInput():
 #Load JSON data (one line currently)
 def jsLoad(userFile):
     try:
+        jsonData = []
+        #opens JSON file
         with open(userFile, 'r') as f:
-            jsonData = json.load(f)
-    #pprint(jsonData) - need to import the file module too
+        #iterates through the malformed JSON to create a list object
+            for line in f:
+                jsonData.append(json.loads(line))
+                #iterate over the list and pass each object to jsParse for parsing/output
+                for i in jsonData:
+                    j = i
+                    jsParse(i,j)
         return jsonData
     except IOError:
         print ('An error occured trying to read data from the file.')
     
 #Function to parse the JSON into attributes
-def jsParse(jsonData):
-    
+def jsParse(jsonData,j):
+    print j
     #Grabs the payload from the request body
     reqBody = jsonData['request']['body']
-    
+
     #Removes the three trailing milliseconds, and converts to local timestamp
     epoch = str(jsonData['tcp']['connectionStartNs'])[0: 10]
     epoch = time.strftime("%a, %b %d %Y %H:%M:%S %Z", time.localtime(float(epoch)))
@@ -42,15 +49,20 @@ def jsParse(jsonData):
     decodedReqBody = unquote(base64.decodestring(reqBody))
     
     #Print out values (static for now, change to requested in the future)
-    print "\nTimestamp: " + epoch + "\n"
-    print "Source IP: " + jsonData['ip']['srcIp'] + "\n"
-    print "Destination IP: " + jsonData['ip']['dstIp'] + "\n"
-    print "Method: " + jsonData['request']['method'] + "\n"
-    print "Full URL Path - Host: " + jsonData['request']['url']['host'] + "  Path: " + jsonData['request']['url']['path'] + "\n"
-    print "Decoded Payload: " + decodedReqBody + "\n"
+    timeStamp = epoch
+    srcIP = jsonData['ip']['srcIp']
+    dstIP = jsonData['ip']['dstIp']
+    method = jsonData['request']['method']
+    fullPath = jsonData['request']['url']['host'] + jsonData['request']['url']['path']
+    decPayload = decodedReqBody
+    #writer.writerow({'Timestamp': timeStamp, 'Source IP': srcIP, 'Destination IP': dstIP, 'Method': method, 'Full URL Path': fullPath, 'Decoded Payload': decPayload})
        
-       
+#output to CSV
+#with open('converted_csv_output.csv', mode='w') as csv_file:
+#    fieldnames = ['Timestamp', 'Source IP', 'Destination IP', 'Method','Full URL Path','Decoded Payload']            writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+#    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+#    writer.writeheader()
+
 #Execute functions
 userFile = userInput()
 jsonData = jsLoad(userFile)
-jsParse(jsonData)
